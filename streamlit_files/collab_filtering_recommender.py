fc19dcf3d2dab7413113c1145d447e2149970e5a
Layla Nyrabia <lnyrabia@gmail.com>
2024-09-20 12:01:49 +0200

    streamlit app

---
+++ streamlit_files/collab_filtering_recommender.py
@@ -0,0 +1,42 @@
+
+import pandas as pd
+import streamlit as st
+
+# Recommendation function based on collaborative filtering
+@st.cache_data
+def recommender_cf(liked_books, streamlit_df):
+    # Create data frame of unique users who like the same books as user
+    filtered_ratings = streamlit_df[streamlit_df['isbn'].isin(liked_books) & (streamlit_df['individual_rating'] > 8)]
+    overlap_users = set()
+    # Remove duplicated rows (if any) where book-user-rating are all identical
+    # Keep only the columns 'user_id', 'isbn', 'book_title', 'individual_rating'
+    overlap_users = set(filtered_ratings.apply(
+        lambda row: (row['user_id'], row['isbn'],
+                     row['book_title'], row['individual_rating']), axis=1))
+    overlap_users = pd.DataFrame(list(overlap_users), 
+                                 columns=['user_id', 'isbn', 
+                                          'book_title', 'individual_rating'])
+    overlap_users['isbn'] = overlap_users['isbn'].astype(str)
+
+    # overlap_users contains these users' ratings of OUR liked_books
+    # now we need to add all the OTHER books they liked (rating > 8)
+    # and remove all our liked_books to get fresh recommendations
+
+    total_books = streamlit_df[streamlit_df['user_id'].isin(overlap_users['user_id'])]
+                                        # All the books they've rated
+    total_books = total_books[total_books['individual_rating'] > 8]
+                                        # All the books they liked
+    recommended = total_books[~total_books['isbn'].isin(liked_books)]
+                                        # Only new books to our user
+    recommended = recommended.drop_duplicates(subset='isbn') # dedupl. books
+    recommended_2 = recommended['isbn'].value_counts().head(10).reset_index()
+                                        # 10 books that were most often
+                                        # rated > 8 by overlapping users
+                                        # each row 1 indiv. rating > 8
+                                        # of all the other books they like
+    recommended_2.columns = ['isbn', 'count']
+    recommended_with_titles = pd.merge(recommended_2, 
+                                       total_books[['isbn', 'book_title', 
+                                                    'image_url_m']], 
+                                                    on='isbn', how='left')
+    return recommended_with_titles[['isbn', 'book_title', 'image_url_m']]
--- streamlit_files/loading_datasets.py
+++ streamlit_files/loading_datasets.py
@@ -1,7 +1,9 @@
 
 import pandas as pd
+import streamlit as st
 
 # Load data function
+# @st.cache_data
 def load_data():
     books_ratings = pd.read_csv("../data/books_rated.csv", 
                                 sep=";", encoding="utf-8-sig")
@@ -9,7 +11,9 @@ def load_data():
                                       sep=";", encoding="utf-8-sig")
     raters_15plus = pd.read_csv("../data/raters_15plus.csv", 
                                 sep=";", encoding="utf-8-sig")
-    return books_ratings, books_users_ratings, raters_15plus
+    streamlit_df = pd.read_csv("../data/streamlit_df.csv", 
+                                sep=";", encoding="utf-8-sig")
+    return books_ratings, books_users_ratings, raters_15plus, streamlit_df
 # print("To load the dataframes, run this line of code:")
 # print('books_ratings, books_users_ratings, raters_15plus = load_data()')
 
