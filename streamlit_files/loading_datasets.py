7efcd3429eb39d228b88ff166ba5da71098b7da2
Ivan <john.weaver.dev@gmail.com>
2024-09-20 12:17:25 +0200

    improve data paths 4

--- streamlit_files/loading_datasets.py
+++ streamlit_files/loading_datasets.py
@@ -11,13 +11,14 @@ import os
 data_folder = os.path.join(os.path.dirname(__file__), "..", "data")
 
 books_ratings_path = os.path.join(data_folder, "books_rated.csv")
+books_users_ratings_path = os.path.join(data_folder, "books_users_ratings.csv")
 raters_15plus = os.path.join(data_folder, "raters_15plus.csv")
 streamlit_df_path = os.path.join(data_folder, "streamlit_df.csv")
 
 def load_data():
     books_ratings = pd.read_csv(books_ratings_path, 
                                 sep=";", encoding="utf-8-sig")
-    books_users_ratings = pd.read_csv("../data/books_users_ratings.csv", 
+    books_users_ratings = pd.read_csv(books_users_ratings_path, 
                                       sep=";", encoding="utf-8-sig")
     raters_15plus = pd.read_csv(raters_15plus, 
                                 sep=";", encoding="utf-8-sig")
